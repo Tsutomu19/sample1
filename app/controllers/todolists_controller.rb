deee1ecc9041ce074812b913f70147a1ce2d25d5
竹田勤 <takedatsutomu@taketakinnoMacBook-puro.local>
2020-02-06 19:50:22 +0900

    [finish]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,16 +1,28 @@
 class TodolistsController < ApplicationController
-  def new
-  	#Viewへ受け渡すためのインスタンス変数にからのモデルオブジェクトを生成する
-  	@list = List.new
-  end
-  def create
-  	list  = List.new(list_params)
-  	list.save
-  	redirect_to '/top'
-  end
-  private
 
-  def list_params
-  	params.repuire(:list).permit(:title,:body)
-  end
+  	def new
+  	#Viewへ受け渡すためのインスタンス変数にからのモデルオブジェクトを生成する
+  		@list = List.new
+  	end
+
+	def index
+		@lists = List.all
+	end
+
+	def show
+		@list = List.find(params[:id])
+	end
+
+	def create
+  		list = List.new(list_params)#ストロングパラメーターを利用
+  		list.save #DBへ保存
+
+  		redirect_to todolist_path(list.id)
+  	end
+
+private
+
+  	def list_params
+  	params.require(:list).permit(:title, :body)
+  	end
 end
